2a09564495ce1e947b1b7844794643a5b0c94fbb
ilham <ilham.hidayatullah18@gmail.com>
2024-10-17 05:49:45 +0700

    switch alarm

--- lib/presentation/getx/alarm/alarm.dart
+++ lib/presentation/getx/alarm/alarm.dart
@@ -1,3 +1,27 @@
 import 'package:get/get.dart';
+import 'package:medics/core/utils/helpers/alarm_helper.dart';
+import 'package:medics/core/utils/helpers/shared_preference.dart';
 
-class AlarmController extends GetxController {}
+class AlarmController extends GetxController {
+  RxInt faseValue = 1.obs;
+
+  @override
+  void onInit() async {
+    super.onInit();
+
+    int? fase = await SharedPreferencesHelper.getFase();
+
+    faseValue.value = fase!;
+  }
+
+  void changeFase(int fase) async {
+    faseValue.value = fase;
+    await SharedPreferencesHelper.saveFase(fase);
+
+    AlarmHelper alarm = AlarmHelper();
+
+    await alarm.scheduleAlarm();
+
+    Get.back();
+  }
+}
